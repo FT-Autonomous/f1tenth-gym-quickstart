dbfac0f29c04ffaa55b6585edeb79c937b5a603a
David Nugent <david.nugent2425@gmail.com>
2021-03-30 01:28:16 +0100

    removed redundant code

--- simulator.py
+++ simulator.py
@@ -5,16 +5,15 @@ import gym
 import numpy as np
 from argparse import Namespace
 
+
 if __name__ == '__main__':
 
-    work = {'mass': 3.463388126201571, 'lf': 0.15597534362552312,
-            'tlad': 0.82461887897713965, 'vgain': 0.90338203837889}
     with open('config_example_map.yaml') as file:
         conf_dict = yaml.load(file, Loader=yaml.FullLoader)
     conf = Namespace(**conf_dict)
 
     env = gym.make('f110_gym:f110-v0', map=conf.map_path,
-            map_ext=conf.map_ext, num_agents=1)# s_min=-np.pi, s_max=np.pi)
+            map_ext=conf.map_ext, num_agents=1)
     obs, step_reward, done, info = env.reset(np.array([[conf.sx, conf.sy,
         conf.stheta]]))
     env.render()
