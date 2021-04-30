493d9ad9a442f2fd3b1649f62894ddd1a0e385a6
David Nugent <david.nugent2425@gmail.com>
2021-04-30 22:33:30 +0100

    added comments to starting point drivers

--- src/starting_point.py
+++ src/starting_point.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+
+# drives straight ahead at a speed of 5
 class SimpleDriver:    
 
     def process_lidar(self, ranges):
@@ -7,14 +9,21 @@ class SimpleDriver:
         steering_angle = 0.0
         return speed, steering_angle
 
+
+# drives toward the furthest point it sees
 class AnotherDriver:
 
     def process_lidar(self, ranges):
+        # the number of LiDAR points
         NUM_RANGES = len(ranges)
+        # angle between each LiDAR point
         ANGLE_BETWEEN = 2*np.pi / NUM_RANGES
+        # number of points in each quadrant
         NUM_PER_QUADRANT = NUM_RANGES // 4
 
+        # the index of the furthest LiDAR point (ignoring the points behind the car)
         max_idx = np.argmax(ranges[NUM_PER_QUADRANT:-NUM_PER_QUADRANT]) + NUM_PER_QUADRANT
+        # some math to get the steering angle to correspond to the chosen LiDAR point
         steering_angle = max_idx*ANGLE_BETWEEN - (NUM_RANGES//2)*ANGLE_BETWEEN
         speed = 5.0
         
