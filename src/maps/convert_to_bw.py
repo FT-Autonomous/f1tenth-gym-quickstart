b53f003ea190863e6f3444f105543830134dbe93
David Nugent <david.nugent2425@gmail.com>
2021-04-30 19:34:08 +0100

    added graphics

--- src/maps/convert_to_bw.py
+++ src/maps/convert_to_bw.py
@@ -1,8 +1,7 @@
 import cv2
 
-PATH = 'OBSTACLES.png'
-
 if __name__ == '__main__':
+    PATH = input('Enter name of RGB file: ')
     img = cv2.imread(PATH, -1)
     img_bw = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.imwrite(PATH, img_bw)
